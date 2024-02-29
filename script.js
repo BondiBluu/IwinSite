961b0cda3eb5122fc7627dfd2b088e24341fd962
Bami O. <bamideleogundeji949@gmail.com>
2024-02-29 10:41:37 -0500

    Add script.js and update video player layout

---
+++ script.js
@@ -0,0 +1,9 @@
+function slideRight() {
+  var videoSlider = document.getElementById('video-slider');
+  videoSlider.scrollLeft += 600;
+}
+
+function slideLeft() {
+    var videoSlider = document.getElementById('video-slider');
+    videoSlider.scrollLeft -= 600;
+  }
